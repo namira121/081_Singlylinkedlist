85b9db2921a0622dcc34e303097085d7eb0588b4
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-19 15:13:06 +0700

    Add project files.

---
+++ LinkedListOOP/LinkedListOOP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+public:
+	int noMhs;
+	char nama[20];
+	Node* next;
+};
+
+class List
+{
+	Node* START;
+public:
+	List();
+	void addNode();
+	bool Search(int nim, Node** current, Node** previous);
+	bool listEmpty();
+	bool delNode(int element);
+	void traverse();
+};
