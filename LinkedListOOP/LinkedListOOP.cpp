e69026a756798077c981596538706cac13eaccf3
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-19 15:23:47 +0700

    membuat class Node

--- LinkedListOOP/LinkedListOOP.cpp
+++ LinkedListOOP/LinkedListOOP.cpp
@@ -21,3 +21,29 @@ public:
 	bool delNode(int element);
 	void traverse();
 };
+
+List::List()
+{
+	START = NULL;
+}
+void List::addNode()/*Menambahkan sebuah Node kedalam list*/
+{
+	int nim;
+	char nm[20];
+	cout << "\nMasukkan Nomor Mahasiswa: ";
+	cin >> nim;
+	cout << "\nMasukkan Nama: ";
+	cin >> nm;
+
+	Node* nodeBaru = new Node;
+	nodeBaru->noMhs = nim;
+	strcpy_s(nodeBaru->nama, nm);
+
+	if (START == NULL || nim <= START->noMhs)
+	{
+		if ((START != NULL) && (nim == START->noMhs))
+		{
+
+		}
+	}
+}
